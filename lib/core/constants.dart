3642073ba9d0b121f5b9bf8d38ba4ed813502cc7
Tommy van Kessel <github@tommyvankessel.nl>
2025-02-13 15:06:55 +0100

    Added --version flag

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,28 @@
+import 'dart:io' show File, Directory;
+import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
+
 /// Constants used throughout the Neo CLI
 class NeoConstants {
+  /// The current version of the Neo CLI
+  static String get version {
+    try {
+      String packageRoot = Directory.current.path;
+      final pubspecPath = path.join(packageRoot, 'pubspec.yaml');
+      final pubspecFile = File(pubspecPath);
+
+      if (!pubspecFile.existsSync()) {
+        return 'unknown';
+      }
+
+      final pubspecContent = pubspecFile.readAsStringSync();
+      final pubspec = loadYaml(pubspecContent) as Map;
+      return pubspec['version'] as String;
+    } catch (e) {
+      return 'unknown';
+    }
+  }
+
   /// The Neo CLI logo
   static const String logo = '''
 ⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣿⣿⣿⣿⣿⣿⣿⣿⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀

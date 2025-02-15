f649b0fd8af99521880a694f6f0e93e97c63181e
Tommy van Kessel <github@tommyvankessel.nl>
2025-02-15 23:16:32 +0100

    Last minute fixes

--- bin/neo_cli.dart
+++ bin/neo_cli.dart
@@ -3,15 +3,24 @@ import 'package:neo_cli/commands/commands.dart';
 import 'package:neo_cli/core/constants.dart';
 
 void main(List<String> arguments) {
-  // Handle version flag directly
-  if (arguments.contains('--version') || arguments.contains('-v')) {
+  // Create command runner with global flags
+  final runner = CommandRunner('neo', 'Neo CLI')
+    ..addCommand(CreateCommand())
+    ..addCommand(ConfigCommand())
+    ..argParser.addFlag(
+      'version',
+      abbr: 'v',
+      help: 'Print the current version of the Neo CLI.',
+      negatable: false,
+    );
+
+  // Handle version flag
+  final args = runner.argParser.parse(arguments);
+  if (args['version']) {
     print(NeoConstants.version);
     return;
   }
 
-  // Otherwise, proceed with normal command handling
-  CommandRunner('neo', 'Neo CLI')
-    ..addCommand(CreateCommand())
-    ..addCommand(ConfigCommand())
-    ..run(arguments);
+  // Run the command
+  runner.run(arguments);
 }

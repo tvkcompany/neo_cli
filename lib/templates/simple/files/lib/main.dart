bfb2e7a57680056460c101e85d9c183ae98ec539
Tommy van Kessel <github@tommyvankessel.nl>
2025-11-21 01:34:37 +0100

    Improved templates

--- lib/templates/simple/files/lib/main.dart
+++ lib/templates/simple/files/lib/main.dart
@@ -2,22 +2,30 @@ import 'package:flutter/widgets.dart';
 import 'package:neo/neo.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'router/neo_router.dart';
+import 'router/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final neoRouter = NeoRouter();
-
   NeoInitializer.initialize().then((_) {
     runApp(
       ProviderScope(
-        child: NeoApp(
-          title: "{{_PROJECT_NAME_}}",
-          defaultThemeMode: NeoThemeMode.system,
-          routerConfig: neoRouter.config(),
-        ),
+        child: MyApp(),
       ),
     );
   });
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = ref.watch(routerProvider);
+
+    return NeoApp(
+      title: "{{_PROJECT_NAME_}}",
+      defaultThemeMode: NeoThemeMode.system,
+      routerConfig: appRouter.config(),
+    );
+  }
 }
